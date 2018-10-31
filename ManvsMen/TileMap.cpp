ca1ff2c74a275afe5e7d1707daae0734b9be937c
Alban Juteau <alban.juteau@ecole.ensicaen.fr>
2018-10-31 19:43:45 +0100

    adding dark filter on the map

--- ManvsMen/TileMap.cpp
+++ ManvsMen/TileMap.cpp
@@ -12,7 +12,7 @@ TileMap::~TileMap()
 bool TileMap::load(sf::Vector2u tileSize, int ** matrix, unsigned int width, unsigned int height,sf::Vector2f referencePos)
 {
 	m_tileset = this->_data->assets.GetImage("images/tileSetMap.png");
-	
+	m_vertices.clear();
 	m_vertices.setPrimitiveType(sf::Quads);
 	m_vertices.resize(width*height * 4);
 
@@ -36,39 +36,11 @@ bool TileMap::load(sf::Vector2u tileSize, int ** matrix, unsigned int width, uns
 			quad[2].texCoords = sf::Vector2f((float)(tu + 1)*tileSize.x, (float)(tv + 1)*tileSize.y);
 			quad[3].texCoords = sf::Vector2f((float)tu*tileSize.x, (float)(tv + 1)*tileSize.y);
 
-			quad[0].color = sf::Color(64, 64, 64);
-			quad[1].color = sf::Color(64, 64, 64);
-			quad[2].color = sf::Color(64, 64, 64);
-			quad[3].color = sf::Color(64, 64, 64);
-
-			for (int i = 0; i < 4; i++) {
-				if (Utils::distance(referencePos, quad[i].position) < PLAYERVIEWFIELDRADIUS / 2.0f) {
-					//ray casting
-					sf::Vector2f point = referencePos;
-					float deltaX = referencePos.x - quad[i].position.x;
-					float deltaY = referencePos.y - quad[i].position.y;
-					float speed = 1;
-					float angle = atan2(deltaY, deltaX);
-					while (this->_data->map.isEntityInMapBounds(point)) {
-						if (Utils::distance(point, quad[i].position) < speed) {
-							quad[i].color = sf::Color::White;
-							break;
-						}
-						point.x = point.x - cos(angle)*speed;
-						point.y = point.y - sin(angle)*speed;
-						if (!this->_data->map.isEntityOnPathCell(point)) {
-							quad[i].color = sf::Color(64, 64, 64);
-							break;
-						}
-					}
-				}
-				else {
-					quad[i].color = sf::Color(64, 64, 64);
-				}
-			}
-
+			quad[0].color = sf::Color(255,255,255,128);
+			quad[1].color = sf::Color(255, 255, 255, 128);
+			quad[2].color = sf::Color(255, 255, 255, 128);
+			quad[3].color = sf::Color(255, 255, 255, 128);
 			
-
 		}
 		
 	}
